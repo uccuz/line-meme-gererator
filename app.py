2f0b36f95048fa1e79a9e3a2a3f15e154d43eef8
uccu <uccu@uccu>
2021-06-21 14:40:57 +0800

    add handle_post_msg

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,ImageSendMessage,
+    MessageEvent, PostbackEvent, TextMessage, TextSendMessage,ImageSendMessage,
     TemplateSendMessage, MessageTemplateAction, PostbackTemplateAction, ButtonsTemplate
 )
 
 from bs4 import BeautifulSoup as bs 
 import requests
 import random
+from datetime import timedelta
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
+
+topic_dic = {}
 
 def random_meme():
     url = 'https://memes.tw/wtf'
@@ -43,7 +46,17 @@ def random_channel():
     url  = content[img_num].find(class_='text-black')['href'].replace("/wtf?contest=", "")
     return topic, img_url, describe, url
 
-
+@handler.add(PostbackEvent)
+def handle_post_message(event):
+    user_id = event.source.user_id
+    data = event.postback.data
+    topic_dic[user_id] = data
+    line_bot_api.reply_message(
+          event.reply_token,
+          TextMessage(
+              text = "選擇主題成功 " + data
+          )
+        )
 
 
 @app.route("/callback", methods=['POST'])
@@ -93,7 +106,13 @@ def handle_message(event):
                   )
               ]
           )
-      )
+        )
+    elif event.message.text == "頻道迷因":
+        user_id = event.source.user_id
+        if user_id in topic_dic:
+          reply_msg = TextSendMessage(topic_dic[user_id])
+        else:
+          reply_msg = TextSendMessage("你還沒選擇主題喔!")
     
     line_bot_api.reply_message(event.reply_token, reply_msg)
 
